5f2eb2ee41a22d702e3715bd9ff2e576b9f2514c
Gabriel Souza <gabriel0gsbn@gmail.com>
2024-04-10 14:10:21 -0300

    ✨: Funções para fazer a limpeza das reviews e remoção da stopwords dentro de cada uma
    Classe especifica para realização da remoção das stopwords e importação dela dentro da classe Processamento
    
    Lista de todas as stopswords com base em 27/04/2012

---
+++ src/services/stopwords.py
@@ -0,0 +1,52 @@
+from typing import List
+from ..utils.stopwords_list import my_stop_words
+import re
+from unidecode import unidecode
+
+
+class StopWordsClear:
+    def __init__(self, reviews: List[str]) -> None:
+        self.stopwords = my_stop_words
+        self.reviews = reviews
+
+    @staticmethod
+    def remove_duplicate_characters(text: str) -> str:
+        character_repeat = re.compile(r"(\w)(\1+)")
+        match_substitution = r"\1"
+
+        def replace_repeated_chars(old_word):
+            new_word = character_repeat.sub(match_substitution, old_word)
+            while new_word != old_word:
+                old_word = new_word
+                new_word = character_repeat.sub(match_substitution, old_word)
+            return new_word
+
+        return replace_repeated_chars(text)
+
+    @staticmethod
+    def remove_stopwords(tokens: List[str], stopwords: set[str]) -> str:
+        filtered_tokens = []
+        for token in tokens:
+            corrected_token = StopWordsClear.remove_duplicate_characters(token)
+            if corrected_token.lower() not in stopwords:
+                filtered_tokens.append(corrected_token)
+        return " ".join(filtered_tokens)
+
+    @staticmethod
+    def clean_text(text: str):
+        cleaned_text = unidecode(text)
+        cleaned_text = re.sub(r"\bR\$(?:\s\d+)?(?:[,.]\d+)?\b", "", cleaned_text)
+        cleaned_text = re.sub(r"[^A-Za-z\s]", " ", cleaned_text)
+        return cleaned_text.lower()
+
+    def preprocess_text(self) -> List[str]:
+        preprocessed_texts = []
+        for text in self.reviews:
+            if text is not None:
+                cleaned_text = StopWordsClear.clean_text(text)
+                tokens = cleaned_text.split()
+                text_without_stopwords = StopWordsClear.remove_stopwords(
+                    tokens, self.stopwords
+                )
+                preprocessed_texts.append(text_without_stopwords)
+        return preprocessed_texts
