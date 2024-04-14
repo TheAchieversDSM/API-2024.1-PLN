701788e4f3f2bbc5d7edc8f8a0a9614b1e75c8ee
Gabriel Souza <gabriel0gsbn@gmail.com>
2024-04-14 19:09:38 -0300

    🐛: Correção no recebimento das rotas para array e não mais CSV

--- src/routes/pln.py
+++ src/routes/pln.py
@@ -1,17 +1,18 @@
-from typing import Union
-from fastapi import APIRouter, File, UploadFile
+from typing import List, Union
+from fastapi import APIRouter
 
+from src.controller.hot_topics import GetHotTopicsByCategory
 from src.controller.most_comments import GetMostCommentsList
+from src.schemas.request import Review
 
-from ..controller.hot_topics import GetHotTopicsByCategory
 
 router = APIRouter(prefix="/pln", tags=["pln"])
 
 
 @router.post("/hotTopics/{category}")
-async def hot_topics(category: str, csv: UploadFile = File(), limit: Union[str, int] = "max"):
-    return GetHotTopicsByCategory(category, csv, limit).process_by_categories()
+async def hot_topics(category: str, data: List[Review], limit: Union[str, int] = "max"):
+    return GetHotTopicsByCategory(category, data, limit).process_by_categories()
 
 @router.post("/mostComments/{category}")
-async def most_comments(category: str, csv: UploadFile = File(), limit: Union[str, int] = "max"):
-    return GetMostCommentsList(category, csv, limit).process_by_categories()
+async def most_comments(category: str, data: List[Review], limit: Union[str, int] = "max"):
+    return GetMostCommentsList(category, data, limit).process_by_categories()
