29c2b942562c9891a1c679c1fa4a225c233d8795
Gabriel Souza <gabriel0gsbn@gmail.com>
2024-04-21 15:26:13 -0300

    🐛:  Correção para voltar a ler arquivo csv

--- src/controller/most_comments.py
+++ src/controller/most_comments.py
@@ -1,47 +1,51 @@
+from io import BytesIO
 from typing import List, Union
+from fastapi import UploadFile
 
-from src.schemas.request import Review
 from src.services.processamento import Processamento
+from ..utils.csv import ReadCsv
 
 
-class GetMostCommentsList:
-    def __init__(self, category, data: List[Review], limit: Union[str, int]) -> None:
+class GetMostCommentsList(ReadCsv):
+    def __init__(self, category, csv: UploadFile, limit: Union[str, int]) -> None:
+        super().__init__()
         self.__category = category
-        self.__data = data
-        self.__limit = self._resolve_limit(limit)
+        self.__csv = csv
+        self.__limit = limit
 
     def _resolve_limit(self, limit: Union[str, int]) -> int:
         if isinstance(limit, str) and limit.lower() == "max":
             return None
         else:
-            return int(limit) + 1 if isinstance(limit, int) else None
+            return int(limit)
 
     def _resolver_category(self, category: str) -> List[str]:
         return category.split(",")
 
-    def init_hot_topic(self):
+    def get_csv_file(self):
+        return BytesIO(self.__csv.file.read())
+
+    def init_most_comment(self):
+        df = self.read_csv()
+        limit = self._resolve_limit(self.__limit)
         categories = self._resolver_category(self.__category)
-        reviews = []
-        for review in self.__data:
-            if review.site_category_lv1 in categories:
-                reviews.append(review)
-                if self.__limit is not None and len(reviews) >= self.__limit:
-                    break
-        return reviews
+        if limit is not None:
+            df = df.head(limit)
+        filters = df[df["site_category_lv1"].isin(categories)]
+        return filters
+
+    def _resolver_category(self, category: str) -> List[str]:
+        return category.split(",")
 
     def process_data(self, df):
-        process = Processamento(df).process_data_document_similarity()
+        process = Processamento(df["review_text"]).process_data_document_similarity()
         return process
 
     def process_by_categories(self):
-        reviews = self.init_hot_topic()
+        df = self.init_most_comment()
         categories = self._resolver_category(self.__category)
         results = {}
         for category in categories:
-            category_reviews = [
-                review.review_text
-                for review in reviews
-                if review.site_category_lv1 == category
-            ]
-            results[category] = self.process_data(category_reviews)
+            category_df = df[df["site_category_lv1"] == category]
+            results[category] = self.process_data(category_df)
         return results
