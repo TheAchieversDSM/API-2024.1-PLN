baab07e38d503258b77d29b955834fa945aa0b8c
Gabriel Souza <gabriel0gsbn@gmail.com>
2024-04-21 14:24:07 -0300

    🐛: Correção no metodo de inicialização da DataBase

--- src/config/database.py
+++ src/config/database.py
@@ -1,39 +1,20 @@
-from fastapi import Depends
-from asyncio import current_task
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_scoped_session, AsyncAttrs
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import text
-from .settings import settings
+# orm/session_manager.py
 from typing import AsyncGenerator
+from fastapi import Depends
+from src.config.settings import settings
+from .db.database_session import DatabaseSessionManager
+from sqlalchemy.ext.asyncio import AsyncSession
+from .db.database_session import db_manager
 
-class Base(AsyncAttrs, DeclarativeBase):
-    pass
+async def init_db():
+    await db_manager.init(settings.DATABASE_URI)
 
-_db_conn: AsyncSession = None
+async def finish_db():
+    await db_manager.close()
 
-async def create_session():
-    global _db_conn
-    _db_conn = create_async_engine(
-        str(settings.DATABASE_URI),
-        **settings.SQLALCHEMY_ENGINE_OPTIONS,
-    )
-    
-    async with _db_conn.begin() as conn:
-        await conn.execute(text(f"SET search_path TO {settings.POSTGRES_SCHEMA}"))
-        await conn.run_sync(Base.metadata.create_all)
+def db_conn() -> DatabaseSessionManager:
+    return db_manager
 
-async def finish_session():
-    global _db_conn
-    if _db_conn:
-        await _db_conn.close()
-
-async def get_db() -> AsyncSession:
-    assert _db_conn is not None
-    return _db_conn
-
-async def db_session(engine = Depends(create_session)) -> AsyncGenerator:
-    async_session_factory = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-    async_session = async_scoped_session(async_session_factory, scopefunc=current_task)
-    async with async_session() as session:
+async def db_session(manager=Depends(db_conn)) -> AsyncGenerator[AsyncSession, None]:
+    async with manager.session() as session:
         yield session
