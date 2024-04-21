a0824ada5a26f6a6a6b01c7f066e52efd534a5bc
Gabriel Souza <gabriel0gsbn@gmail.com>
2024-04-21 14:46:52 -0300

    🐛:  Correção na classe de leitura do CSV para async

--- src/utils/csv.py
+++ src/utils/csv.py
@@ -1,19 +1,17 @@
 from abc import ABC, abstractmethod
-from contextlib import redirect_stdout
-from io import StringIO
+from io import BytesIO, StringIO
 import pandas as pd
 from ..utils.dataset_type import dataset_types
 
 
 class ReadCsv(ABC):
     @abstractmethod
-    def get_csv_file(self):
+    async def get_csv_file(self) -> BytesIO:
         pass
 
-    def read_csv(self) -> pd.DataFrame:
-        csv_file = self.get_csv_file()
-        with StringIO(csv_file.read().decode("utf-8")) as csv_data:
-            with redirect_stdout(None):
-                df = pd.read_csv(csv_data, dtype=dataset_types)
+    async def read_csv(self, csv_file: BytesIO) -> pd.DataFrame:
+        with csv_file as file:
+            csv_data = file.read().decode("utf-8")
+            df = pd.read_csv(StringIO(csv_data), dtype=dataset_types, sep=",")
         df = df.dropna(subset=["review_text"])
         return df
