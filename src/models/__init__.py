c477867af093d046bef78cf2e6f8de6729785795
Gabriel Souza <gabriel0gsbn@gmail.com>
2024-05-02 23:25:30 -0300

    ✨: Arquivos de modulo criados e configurados para puxar as importação das pastas

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,5 @@
-from .comments import Comment  # noqa: F401
-from .products import Product  # noqa: F401
-from .sumary import ProductSummary  # noqa: F401
+from .comments import Comment
+from .products import Product
+from .product_sumary import ProductSummary
+from .base_import import BaseImportLog
+from .category_summary import CategorySummary
---
+++ src/schemas/__init__.py
@@ -0,0 +1,5 @@
+from .base_import import BaseImportModel
+from .category_summary import CategorySummaryModel
+from .commet import CommentModel
+from .product import ProductModel
+from .request import Review
