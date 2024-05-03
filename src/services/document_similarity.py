97ca51fadc47fce6b9946119806cfc5a24a697be
Gabriel Souza <gabriel0gsbn@gmail.com>
2024-05-02 23:35:29 -0300

    🐛: Correção da classe para verificar a similharidade dos documentos positivos

--- src/services/document_similarity.py
+++ src/services/document_similarity.py
@@ -2,29 +2,34 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 import numpy as np
 
-
 class DocumentSimilarity:
-    def __init__(self, reviews: list) -> None:
+    def __init__(self, reviews: list, min_df=0.05, max_df=0.95) -> None:
         self.__reviews = reviews
+        self.min_df = min_df
+        self.max_df = max_df
 
-    def gerar_cluster_documentos(self) -> list:
+    def preprocess_reviews(self) -> list:
         preprocessed_reviews = list(set(self.__reviews))
+        return preprocessed_reviews
 
-        tfidf = TfidfVectorizer(min_df=0.0, max_df=1.0, norm="l2", use_idf=True)
+    def generate_document_clusters(self) -> dict:
+        preprocessed_reviews = self.preprocess_reviews()
+
+        tfidf = TfidfVectorizer(min_df=self.min_df, max_df=self.max_df, norm="l2", use_idf=True)
         tfidf_matrix = tfidf.fit_transform(preprocessed_reviews)
 
-        num_clusters = tfidf_matrix.shape[0]
-
-        feature_names = tfidf.get_feature_names_out()
+        num_samples = len(preprocessed_reviews)
+        num_clusters = max(1, int(num_samples ** 0.5))
 
         kmeans = KMeans(n_clusters=num_clusters, random_state=42)
         cluster_labels = kmeans.fit_predict(tfidf_matrix)
 
+        feature_names = tfidf.get_feature_names_out()
         cluster_results = []
         for i in range(num_clusters):
             cluster_indices = np.where(cluster_labels == i)[0]
-            cluster_text = " ".join([preprocessed_reviews[idx] for idx in cluster_indices])
-            
+            cluster_text = [preprocessed_reviews[idx] for idx in cluster_indices]
+
             if len(cluster_indices) > 0:
                 cluster_tfidf_scores = np.asarray(tfidf_matrix[cluster_indices].mean(axis=0)).flatten()
                 top_keywords_indices = np.argsort(cluster_tfidf_scores)[::-1][:5]
@@ -32,10 +37,12 @@ class DocumentSimilarity:
             else:
                 top_keywords = []
 
-            cluster_results.append({
-                "Cluster Label": i,
-                "Cluster Name": cluster_text,
-                "Top Keywords": ", ".join(top_keywords)
-            })
+            if cluster_text:
+                cluster_results.append({
+                    "Cluster Label": i,
+                    "Cluster Text": cluster_text,
+                    "Top Keywords": ", ".join(top_keywords)
+                })
 
         return cluster_results
+
